666f26143e1a795ce6163f5985c91bb0de83bee4
gopallchoudhary <gopalchoudhary521@gmail.com>
2024-10-21 22:43:35 +0530

    added sc close

--- Questions/Variables and DataTypes/Average.java
+++ Questions/Variables and DataTypes/Average.java
@@ -10,6 +10,7 @@ public class Average {
         int c = sc.nextInt();
         int avg = (a+b+c)/3;
         System.out.println("Average of three subjects is " + avg);
+        sc.close();
 
     }
 }
